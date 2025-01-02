25f4f46d7ff0a9543d4f139b1ce1214d6586f6b5
loharmanish <loharmanish9571@gmail.com>
2025-01-02 11:45:59 +0530

    refactor

--- src/main/java/org/acme/v1/repositories/EmployeeRepository.java
+++ src/main/java/org/acme/v1/repositories/EmployeeRepository.java
@@ -1,4 +1,14 @@
 package org.acme.v1.repositories;
 
-public interface EmployeeRepository {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.v1.models.EmployeeModel;
+
+import java.util.List;
+
+@ApplicationScoped
+public class EmployeeRepository implements PanacheRepository<EmployeeModel> {
+	public List<EmployeeModel> findByCompanyId(Long companyId) {
+		return list("companyId", companyId);
+	}
 }
--- src/main/java/org/acme/v1/resourceHandlers/CompanyResourceHandler.java
+++ src/main/java/org/acme/v1/resourceHandlers/CompanyResourceHandler.java
@@ -1,4 +1,36 @@
 package org.acme.v1.resourceHandlers;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.v1.models.CompanyModel;
+import org.acme.v1.models.EmployeeModel;
+import org.acme.v1.repositories.CompanyRepository;
+import org.acme.v1.repositories.EmployeeRepository;
+import org.acme.v1.resources.CompanyResponseResource;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@ApplicationScoped
 public class CompanyResourceHandler {
+	public final CompanyRepository repository;
+	public final EmployeeRepository employeeRepository;
+
+	@Inject
+	public CompanyResourceHandler(CompanyRepository repository, EmployeeRepository employeeRepository) {
+		this.repository = repository;
+		this.employeeRepository = employeeRepository;
+	}
+
+	public CompanyResponseResource getCompanyDetails(Long id) {
+		CompanyModel model = repository.findById(id);
+		if (model != null) {
+			CompanyResponseResource companyResponseResource = new CompanyResponseResource(model);
+			List<EmployeeModel> model1 = employeeRepository.findByCompanyId(model.getId());
+			companyResponseResource.setEmployeeDetails(model1);
+			companyResponseResource.setNumberOfEmployee(BigInteger.valueOf(1));
+			return companyResponseResource;
+		}
+		return null;
+	}
 }
