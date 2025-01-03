fb1d0ff5bc521c544e7d5588ea91af355807f8e7
loharmanish <loharmanish9571@gmail.com>
2025-01-03 10:53:50 +0530

    added employeeResource and added Api path

--- src/main/java/org/acme/v1/controllers/EmployeeController.java
+++ src/main/java/org/acme/v1/controllers/EmployeeController.java
@@ -1,4 +1,26 @@
 package org.acme.v1.controllers;
 
+import jakarta.enterprise.inject.build.compatible.spi.Validation;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.acme.v1.resources.EmployeeResource;
+import org.jboss.resteasy.reactive.RestResponse;
+
+
+@Path("v1/employee")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class EmployeeController {
+	@POST
+	public RestResponse<?> createUpdate(EmployeeResource resource) {
+		return RestResponse.ResponseBuilder
+				.create(RestResponse.Status.OK, resource)
+				.type(MediaType.APPLICATION_JSON)
+				.build();
+	}
+
+
 }
